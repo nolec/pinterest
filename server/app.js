0b6ca11291e00060d13151c96cfdce67ebbb1b90
nohseungrae <ca10205@naver.com>
2019-12-26 14:14:48 +0900

    hello

--- server/app.js
+++ server/app.js
@@ -21,19 +21,16 @@ app.use("/api/users", userRoute);
 app.use("/api/profile", profileRoute);
 app.use("/api/post", postRoute);
 // app.use("/api/video", videoRoute);
-console.log(__dirname);
 app.use("/uploads", express.static("uploads"));
-// app.use("/nolla", express.static(path.resolve(__dirname, "../client/build")));
+// app.use("/", express.static(path.resolve(__dirname, "../client/build")));
 // app.get("*", (req, res) => {
 //   res.sendFile(path.resolve(__dirname, "../client/build/index.html"));
 // });
 //====================================================
+console.log(__dirname);
 if (process.env.NODE_ENV === "production") {
   //Set static folder
-  app.use(
-    "/pinterest",
-    express.static(path.resolve(__dirname, "../client/build"))
-  );
+  app.use("/", express.static(path.resolve(__dirname, "../client/build")));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../client/build/index.html"));
   });
